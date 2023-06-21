bfd996e9901c605bc053644644ed5d0f4744b474
BeeHeather <jeefury@gmail.com>
2023-06-22 00:57:16 +0300

    add chrome options

--- login_test.py
+++ login_test.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
@@ -9,7 +10,11 @@ LOGIN = 'standard_user'
 PASSWORD = 'secret_sauce'
 
 def get_driver():
-    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+    chrome_options = Options()
+    chrome_options.add_argument("--window-size=1920,800")
+    chrome_options.add_argument("--headless")
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options)
+    driver.implicitly_wait(5)
     return driver
 
 
